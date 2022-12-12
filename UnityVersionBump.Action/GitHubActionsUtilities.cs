56274781076a50c6792d5e27f841c80a71c15933
Vincent Mahnke <vincent@mahn.ke>
2022-12-12 02:01:25 +0100

    chore: Updated to new GITHUB_OUTPUT syntax

--- UnityVersionBump.Action/GitHubActionsUtilities.cs
+++ UnityVersionBump.Action/GitHubActionsUtilities.cs
@@ -2,10 +2,17 @@ public static class GitHubActionsUtilities
 {
     public static void SetOutputVariable(string variable, string value)
     {
-        Console.WriteLine($"::set-output name={variable}::{value}");
+        var output = $"{variable}={value}";
+        var outputFilename = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+        if (outputFilename == null)
+        {
+            Console.WriteLine($"Skipping writing output variable as no GITHUB_OUTPUT environment variable is set: '{output}'");
+            return;
+        }
+        File.WriteAllText(outputFilename, output);
     }
     public static void GitHubActionsWriteLine(string line)
     {
-        Console.WriteLine($"echo {line}");
+        Console.WriteLine($"{line}");
     }
 }
